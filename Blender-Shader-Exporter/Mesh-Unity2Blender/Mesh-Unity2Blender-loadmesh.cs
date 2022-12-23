be62515a0e74a4ea6994a9a05209e7d248fc61b3
Komeil Majidi <k.majidi@esmcell.com>
2022-12-23 18:40:24 +0330

    Update Mesh-Unity2Blender-loadmesh.cs
    Co-Authored-By: Shayna Fahimi <80590738+shaynafahimi@users.noreply.github.com>

--- Blender-Shader-Exporter/Mesh-Unity2Blender/Mesh-Unity2Blender-loadmesh.cs
+++ Blender-Shader-Exporter/Mesh-Unity2Blender/Mesh-Unity2Blender-loadmesh.cs
@@ -36,3 +36,36 @@ public class LoadVertMesh : MonoBehaviour
 
     }
 }
+
+public static class MeshSaverEditor {
+
+	[MenuItem("CONTEXT/MeshFiltering/Save Mesh...")]
+	public static void SaveMeshInPlace (MenuCommand menuCommand) {
+		MeshFilter mf = menuCommand.context as MeshFilter;
+		Mesh m = mf.sharedMesh;
+		SaveMesh(m, m.name, false, true);
+	}
+
+	[MenuItem("CONTEXT/MeshFiltering/Save Mesh As New Instance")]
+	public static void SaveMeshNewInstanceItem (MenuCommand menuCommand) {
+		MeshFilter mf = menuCommand.context as MeshFilter;
+		Mesh m = mf.sharedMesh;
+		SaveMesh(m, m.name, true, true);
+	}
+
+	public static void SaveMesh (Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh) {
+		string path = EditorUtility.SaveFilePanel("Save Mesh Asset", "Assets/", name, "asset");
+		if (string.IsNullOrEmpty(path)) return;
+        
+		path = FileUtil.GetProjectRelativePath(path);
+
+		Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
+		
+		if (optimizeMesh)
+		     MeshUtility.Optimize(meshToSave);
+        
+		AssetDatabase.CreateAsset(meshToSave, path);
+		AssetDatabase.SaveAssets();
+	}
+	
+}
